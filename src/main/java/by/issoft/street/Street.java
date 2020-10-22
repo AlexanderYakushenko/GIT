c3ed40027e334ca0b4ee155b7816f1ffa4004281
AlexanderYakushenko <alexanderyakushenko@coherentsolutions.com>
2020-10-22 21:04:14 +0300

    first commit

---
+++ src/main/java/by/issoft/street/Street.java
@@ -0,0 +1,38 @@
+package by.issoft.street;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Street {
+
+     public static void main(String[] args) {}
+//
+//
+//        public static street(){
+//
+//             //List<House>  houses = new ArrayList<>(new House(90.5,4));
+//                //ArrayList houses = new ArrayList<>(new House(90.5,4));
+//
+//                Class Street = new Street(){ List<House> houses ...}.
+//
+//
+//                //Class House { list<flat> flats.
+                //Class flat{ double square. Int num....}
+//                House multiApp = new House();
+//                House tawnhouse = new House();
+//                House privHouse = new House();
+
+
+
+
+
+
+
+
+
+
+
+
+}
