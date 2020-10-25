3bd4d3ec7efbfe6fac5fc86b3cf6e11f5299cfc9
WIN-6QQSBAVVN64\User <alexanderyakushenko@coherentsolutions.com>
2020-10-25 22:06:31 +0300

    adding a new project

---
+++ src/main/java/by/issoft/relocate/MainClass.java
@@ -0,0 +1,13 @@
+//package by.issoft.relocate;
+//
+//public class MainClass {
+//
+//    public static void main(String[] args) {
+//
+//        employee.relocateToVilnus(){
+//            for(add.euOffice()(remove.orfficeBY()))
+//                throw exeption ()
+//        }
+//
+//    }
+//}
--- src/main/java/by/issoft/street/Street.java
+++ src/main/java/by/issoft/street/Street.java
@@ -1,24 +1,27 @@
 package by.issoft.street;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Street {
 
-     String name;
-     List<House> houses;
+    String name;
+    List<House> houses;
 
-     public Street(String name) {
-          this.name = name;
-          this.houses = new ArrayList<>();
-     }
+    public Street(String name) {
+        this.name = name;
+        this.houses = new ArrayList<>();
+    }
+
     public void addHouse(House house) {
-          houses.add(house);
-     }
-     public double calculatePrice() {
-          double price = 0;
-          for (House house : houses){
-               price += house.calculatePrice();
-          }
-          return price;
-     }
+        houses.add(house);
+    }
+
+    public double calculatePrice() {
+        double price = 0;
+        for (House house : houses) {
+            price += house.calculatePrice();
+        }
+        return price;
+    }
 }
